a3ad2ffc5211b730011525279021cfee0579f661
조충은 <agger2154@gmail.com>
2020-04-16 20:28:16 +0900

    [WIP] apollo client 세팅중

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,21 @@
 import App from 'next/app'
+import { ApolloClient, NormalizedCacheObject } from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+import withApolloClient from 'with-apollo-client'
 
-class MyApp extends App {
+interface AppProps {
+  apolloClient: ApolloClient<NormalizedCacheObject>
+}
+
+class MyApp extends App<AppProps> {
   public render(): JSX.Element {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, apolloClient } = this.props
     return (
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     )
   }
 }
 
-export default MyApp
+export default withApolloClient(MyApp)
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -30,7 +30,18 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta httpEquiv="Content-Type" content="text/html" charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            href="/static/favicon.ico"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
