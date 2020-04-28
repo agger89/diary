3d2d01029bb939597e77560ccef0a383dff9d7e8
조충은 <agger2154@gmail.com>
2020-04-28 20:29:31 +0900

    login 화면단 작업중

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import LoginScreen from 'screens/LoginScreen'
+
+const LoginPage: FunctionComponent = () => {
+  return <LoginScreen />
+}
+
+export default LoginPage
---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import LoginForm from './LoginForm'
+
+const LoginScreen: FunctionComponent = () => {
+  return <LoginForm />
+}
+
+export default LoginScreen
