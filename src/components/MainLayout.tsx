f4d00cbaa32e858f2f26755d165afdaa7db15b3f
조충은 <agger2154@gmail.com>
2020-05-06 18:26:54 +0900

    Add MainLayout style

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,5 +1,13 @@
 import React, { FunctionComponent, ReactNode } from 'react'
+import styled from 'styled-components'
+
 import BottomNav from './BottomNav'
+
+const LayoutBlock = styled.div`
+  position: relative;
+  width: 600px;
+  margin: 0 auto;
+`
 
 interface MainLayoutProps {
   children: ReactNode
@@ -7,10 +15,10 @@ interface MainLayoutProps {
 
 const MainLayout: FunctionComponent<MainLayoutProps> = ({ children }) => {
   return (
-    <>
+    <LayoutBlock>
       {children}
       <BottomNav />
-    </>
+    </LayoutBlock>
   )
 }
 
