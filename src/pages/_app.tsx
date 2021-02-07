f749c9951259c3c0f6cc038f1daed03db77b5204
조충은 <agger2154@gmail.com>
2021-02-07 21:29:08 +0900

    node version 업데이트, .nvmrc 파일 추가, apollo 초기 세팅 예전 버전 걷어내고 최신으로 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,19 @@
-import App from 'next/app'
-import { ApolloClient, NormalizedCacheObject } from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
-import withApolloClient from 'with-apollo-client'
+import { AppProps } from 'next/app'
+import { FC } from 'react'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '../libs/apolloClient'
 
 import GlobalStyles from '../GlobalStyles'
 
-interface AppProps {
-  apolloClient: ApolloClient<NormalizedCacheObject>
+const MyApp: FC<AppProps> = (props) => {
+  const { Component, pageProps } = props
+  const apolloClient = useApollo(pageProps)
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ApolloProvider>
+  )
 }
 
-class MyApp extends App<AppProps> {
-  public render(): JSX.Element {
-    const { Component, pageProps, apolloClient } = this.props
-
-    return (
-      <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
-        <GlobalStyles />
-      </ApolloProvider>
-    )
-  }
-}
-
-export default withApolloClient(MyApp)
+export default MyApp
