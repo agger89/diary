780848a96157d35096450ced7a425a417fc6de41
조충은 <agger2154@gmail.com>
2021-02-15 23:57:57 +0900

    login modal button style 작업중

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -22,6 +22,14 @@ const LoginModalBlock = styled.div`
   background-color: #1c1e21;
   border: 1px solid #f4f5f61f;
   border-radius: 16px;
+  .title {
+    padding: 30px 0px;
+    font-weight: 700;
+    font-size: 24px;
+    color: #fff;
+    letter-spacing: 1.5px;
+    text-align: center;
+  }
   .close-button {
     position: absolute;
     top: 10px;
@@ -43,6 +51,25 @@ const CloseIcon = styled(Close)`
   cursor: pointer;
 `
 
+const ButtonBlock = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  button {
+    width: 200px;
+    margin-bottom: 14px;
+    padding: 14px 24px;
+    border: 0;
+    border-radius: 10px;
+    box-shadow: 0;
+    outline: none;
+    cursor: pointer;
+    span {
+      font-size: 16px;
+      font-weight: 700;
+    }
+  }
+`
 interface LoginProps {
   onToggleLoginModal: (value: boolean) => void
 }
@@ -52,10 +79,18 @@ const Login: FunctionComponent<LoginProps> = ({ onToggleLoginModal }) => {
     <ModalWrap>
       <LoginBlock>
         <LoginModalBlock>
-          login modal
           <button className="close-button" onClick={() => onToggleLoginModal(false)}>
             <CloseIcon />
           </button>
+          <h1 className="title">Diary</h1>
+          <ButtonBlock>
+            <button>
+              <span>Sign in with GitHub</span>
+            </button>
+            <button>
+              <span>Sign in with Google</span>
+            </button>
+          </ButtonBlock>
         </LoginModalBlock>
       </LoginBlock>
     </ModalWrap>
