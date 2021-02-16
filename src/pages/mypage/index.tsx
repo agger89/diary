77c9e082f2506d0890da8710a4405427b08a616f
조충은 <agger2154@gmail.com>
2021-02-16 23:01:52 +0900

    mypage 컴포넌트 생성

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+import PageLayout from 'components/PageLayout'
+import MypageScreen from 'screens/MypageScreen'
+
+const MypagePage: FunctionComponent = () => {
+  return (
+    <PageLayout>
+      <MypageScreen />
+    </PageLayout>
+  )
+}
+
+export default MypagePage
---
+++ src/screens/MypageScreen/MypageScreen.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react'
+
+const MypageScreen: FunctionComponent = () => {
+  return <div>MypageScreen</div>
+}
+
+export default MypageScreen
