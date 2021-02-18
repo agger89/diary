41861581def6229e6c45aed11c793168b45b1986
조충은 <agger2154@gmail.com>
2021-02-18 22:56:25 +0900

    props로 profile image border-radius 세팅

--- src/components/ProfileImage/ProfileImage.tsx
+++ src/components/ProfileImage/ProfileImage.tsx
@@ -9,19 +9,29 @@ const Image = styled.div`
   background-image: ${({ name }) => `url('/static/images/${name}.png')`};
   background-size: contain;
   background-position: center;
-  border-radius: 50%;
+  border-radius: ${({ borderRadius }) => `${borderRadius}px`};
   cursor: pointer;
 `
 
 interface ProfileImageProps {
   width: number
   height: number
+  borderRadius?: number
 }
 
-const ProfileImage: FunctionComponent<ProfileImageProps> = ({ width, height }) => {
+const ProfileImage: FunctionComponent<ProfileImageProps> = ({
+  width,
+  height,
+  borderRadius,
+}) => {
   return (
     <Link href="/user-profile-page">
-      <Image name="mark_zuckerberg" width={width} height={height} />
+      <Image
+        name="mark_zuckerberg"
+        width={width}
+        height={height}
+        borderRadius={borderRadius}
+      />
     </Link>
   )
 }
--- src/screens/MypageScreen/MypageScreen.tsx
+++ src/screens/MypageScreen/MypageScreen.tsx
@@ -58,7 +58,7 @@ const MypageScreen: FunctionComponent = () => {
         <span className="bottom-text">Please fill in your details below</span>
       </HeaderDescriptionBlock>
       <ProfileImageBlock>
-        <ProfileImage width={100} height={100} />
+        <ProfileImage width={100} height={100} borderRadius={15} />
         <LoginInfoBlock>
           <TopTextBlock>
             <Github />
